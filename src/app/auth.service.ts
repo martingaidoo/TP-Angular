39ece707d29dca4971361996748e812436800226
Matias Presuttari <matipresuttari@gmail.com>
2024-06-29 01:41:46 -0300

    agregado refresh token

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { LoginI, RegisterI, TokenI } from './interfaces/token';
 import { HttpErrorResponse } from '@angular/common/http';
+import { timer } from 'rxjs';
+import  moment from 'moment';
 @Injectable({
   providedIn: 'root',
 })
@@ -14,6 +16,10 @@ export class AuthService {
     try {
       const response = (await axios.post(`${this.url}/login`, body)).data;
       localStorage.setItem('token', JSON.stringify(response));
+      const time = moment(response.expirationTime).diff(moment());
+      timer(time*0.5).subscribe(async () => {
+        await this.refreshToken();
+      });
       return response;
     } catch (error) {
       throw new HttpErrorResponse({ error });
@@ -26,6 +32,19 @@ export class AuthService {
     } catch (error) {
       throw new HttpErrorResponse({ error });
     }
+  }
+
+  async refreshToken() {
+    const tokenObject:{refreshToken:string,accessToken:string}=JSON.parse(localStorage.getItem('token')??"{refreshToken:''}")
+    const response = (
+      await axios.get(`${this.url}/refresh-token`, {
+        headers: {
+          'refresh-token': tokenObject.refreshToken
+        },
+      })
+    ).data;
+    tokenObject.accessToken = response.accessToken
+    localStorage.setItem('token', JSON.stringify(tokenObject));
   }
   logout(): void {
     localStorage.removeItem('token')
--- src/app/template/form-products/form-products.component.ts
+++ src/app/template/form-products/form-products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import axios from 'axios';
+
 @Component({
   selector: 'app-form-products',
   standalone: true,
@@ -8,12 +10,17 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './form-products.component.css'
 })
 export class FormProductsComponent implements OnInit {
+
+  url = 'http://localhost:3001';
   id: string='';
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.queryParams['id'];
-      console.log(this.id)
+    console.log(this.id)
+ 
+    axios.get(`${this.url}/products`+this.id)
+  
 
   }
 
